2ae1d20b3b03003c7e261302eff56db9aaff26e2
Adm <adm@MacBook-Pro-Adm.local>
2021-10-20 02:09:57 +0400

    first git repo

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,4 +1,22 @@
 public class Lesson4 {
+    public static void main(String[] args) {
+        byte a = 102;
+        short b=3100;
+        int c =12764;
+        float d=7.3f;
+        long e = 25556788;
+        double f = 54.323;
+        char cc='A';
+        String ss ="text is here";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(cc);
+        System.out.println(ss);
+
     }
 
 
